0bccc143d482f830f28669b0a784778f45920aa2
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2016-05-07 23:07:50 +1000

    Added Zip file from blackboard

---
+++ CurvedLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;           // For Pen.
+using System.Drawing.Drawing2D; // For GraphicsPath.
+
+namespace SimplePaint {
+    class CurvedLine {
+        GraphicsPath graphicsPath = new GraphicsPath();
+        Pen pen;
+
+        public CurvedLine (Pen pen) {
+            this.pen = (Pen)pen.Clone();
+            // Make the internal ends of each line rounded.
+            pen.LineJoin = LineJoin.Round;
+        }
+
+        public void Add(Point startPoint, Point endPoint) {
+            graphicsPath.AddLine(startPoint, endPoint);
+        }
+
+        public void Draw(Graphics graphics) {
+            graphics.DrawPath(pen, graphicsPath);
+        }
+    }
+}
