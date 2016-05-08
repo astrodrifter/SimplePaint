d27a159c5fd5fb0668829a3b9921b49a0ede7d3d
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2016-05-08 10:16:25 +1000

    Q6-7

--- SimplePaint/SimplePaintForm.cs
+++ SimplePaint/SimplePaintForm.cs
@@ -7,9 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D; // For LineCap.
+using System.IO;                // For Directory and Path.
+using System.Drawing.Imaging;   // For ImageFormat.
+using System.Diagnostics;       // For Debug.
+
 
 namespace SimplePaint {
     public partial class SimplePaintForm : Form {
+        Bitmap initialBitmap;
+        Graphics graphics;
+        bool userIsDrawing = false;
+        Point startPoint;
+        Pen pen = new Pen(Color.Fuchsia, 3);  // Pen Width is 3, initially.
+        bool imageIsModified;
+        string currentFilePath;  /* Is empty, when the current image hasn't been opened or saved. */
+
+
         public SimplePaintForm() {
             InitializeComponent();
         }
